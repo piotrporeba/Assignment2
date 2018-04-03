2825a526126f999305e8e1a15f9df7b39d389e08
piotrporeba <B00090928@student.itb.ie>
2018-04-03 20:05:11 +0100

    users fixtures added

--- src/Migrations/Version20180403153358.php
+++ src/Migrations/Version20180403153358.php
@@ -8,14 +8,15 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180220095242 extends AbstractMigration
+class Version20180403153358 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(100) NOT NULL, price NUMERIC(10, 2) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE app_users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(64) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json_array)\', UNIQUE INDEX UNIQ_C2502824F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE product DROP category_id, CHANGE ingredients ingredients VARCHAR(100) NOT NULL');
     }
 
     public function down(Schema $schema)
@@ -23,6 +24,7 @@ class Version20180220095242 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE app_users');
+        $this->addSql('ALTER TABLE product ADD category_id INT DEFAULT NULL, CHANGE ingredients ingredients VARCHAR(100) DEFAULT NULL COLLATE utf8_unicode_ci');
     }
 }
--- src/Migrations/Version20180403153600.php
+++ src/Migrations/Version20180403153600.php
@@ -8,14 +8,15 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180330131301 extends AbstractMigration
+class Version20180403153600 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE product ADD title VARCHAR(100) NOT NULL, ADD summary VARCHAR(100) NOT NULL, ADD photo VARCHAR(100) NOT NULL, ADD ingredients VARCHAR(100) NOT NULL');
+        $this->addSql('CREATE TABLE app_users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(64) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json_array)\', UNIQUE INDEX UNIQ_C2502824F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE product DROP category_id, CHANGE ingredients ingredients VARCHAR(100) NOT NULL');
     }
 
     public function down(Schema $schema)
@@ -23,6 +24,7 @@ class Version20180330131301 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE product DROP title, DROP summary, DROP photo, DROP ingredients');
+        $this->addSql('DROP TABLE app_users');
+        $this->addSql('ALTER TABLE product ADD category_id INT DEFAULT NULL, CHANGE ingredients ingredients VARCHAR(100) DEFAULT NULL COLLATE utf8_unicode_ci');
     }
 }
