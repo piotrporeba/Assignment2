02b057fddb402a6cd32af0741a535d6499d0ae63
piotrporeba <B00090928@student.itb.ie>
2018-04-04 15:22:44 +0100

    access denied exception and logger added

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Security;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+
+    private $twig;
+    private $logger;
+
+    public function __construct(ContainerInterface $container, LoggerInterface $logger) {
+        $this->twig = $container->get('twig');
+        $this->logger = $logger;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $this->logger->error('access denied exception');
+        $template = 'error/accessDenied.html.twig';
+        $args = [];
+        $html = $this->twig->render($template, $args);
+        return new Response($html);
+    }
+}
