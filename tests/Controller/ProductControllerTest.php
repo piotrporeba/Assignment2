82d8009c2b8fdc8edb0b77c28c8e9a92f6c3f1e6
piotrporeba <B00090928@student.itb.ie>
2018-04-23 10:26:29 +0100

    some testing backtraced

--- tests/Controller/ProductControllerTest.php
+++ tests/Controller/ProductControllerTest.php
@@ -6,44 +6,34 @@ use Symfony\Component\HttpFoundation\Response;
 class ProductControllerTest extends WebTestCase
 
 {
-    public function testProductPageResponseCodeMoved() {
-        // Arrange
-        $url = '/product';
-        $httpMethod = 'GET';
-        $client = static::createClient();
-        // Assert
-        $client->request($httpMethod, $url);
-        // Assert
-        $this->assertSame( Response::HTTP_MOVED_PERMANENTLY, $client->getResponse()->getStatusCode() );
-    }
 
-    public function testProductindexPageResponseCodeMoved() {
-        // Arrange
-        $url = '/product/19';
+    public function testProductPageResponseAndContains(){
+            //arrange
+        $url = '/product/';
         $httpMethod = 'GET';
         $client = static::createClient();
-        // Assert
-        $client->request($httpMethod, $url);
-        // Assert
-        $this->assertNotNull($client->getResponse()->getStatusCode() );
-    }
 
-    public function testNewProductPageResponseCodeOk() {
-        // Arrange
-        $url = '/product/new';
-        $httpMethod = 'GET';
-        $client = static::createClient();
-        // Assert
+        $expectedResult = Response::HTTP_OK;
+        $expectedContent='seafood';
+        $expectedContentToLowerCase = strtolower($expectedContent);
+
+        //act
         $client->request($httpMethod, $url);
-        // Assert
-        $this->assertSame( Response::HTTP_OK, $client->getResponse()->getStatusCode() );
+        $statusCode = $client->getResponse()->getStatusCode();
+        $resultContent = $client->getResponse()->getContent();
+        $resultContentToLowerCase = strtolower($resultContent);
+
+        //assert
+        $this->assertSame($expectedResult, $statusCode);
+        $this->assertContains($expectedContentToLowerCase, $resultContentToLowerCase);
     }
 
 
 
+    //////// Code to get something working
     public function testProductRedirectingPageContent() {
         // Arrange
-        $url = '/product/';
+        $url = '/product';
         $httpMethod = 'GET';
         $client = static::createClient();
         $searchText = 'Redirecting';
@@ -54,44 +44,180 @@ class ProductControllerTest extends WebTestCase
         $this->assertContains( $searchText, $client->getResponse()->getContent() );
     }
 
-    public function testProductPageContent() {
+
+    /////// Code copied from previous test version
+    ///
+
+    public function testProductPageResponseCodeMoved() {
+
         // Arrange
-        $url = '/product/{19}';
+
+        $url = '/product';
+
         $httpMethod = 'GET';
+
         $client = static::createClient();
-        $searchText = 'Product';
-        // Act
+
+        // Assert
+
         $client->request($httpMethod, $url);
 
         // Assert
-        $this->assertContains($searchText, $client->getResponse()->getContent() );
+
+        $this->assertSame( Response::HTTP_MOVED_PERMANENTLY, $client->getResponse()->getStatusCode() );
+
     }
+
+
+
+    public function testProductindexPageResponseCodeMoved() {
+
+        // Arrange
+
+        $url = '/product/19';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        // Assert
+
+        $client->request($httpMethod, $url);
+
+        // Assert
+
+        $this->assertNotNull($client->getResponse()->getStatusCode() );
+
+    }
+
+
+
+    public function testNewProductPageResponseCodeFound() {
+
+        // Arrange
+
+        $url = '/product/new';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        // Assert
+
+        $client->request($httpMethod, $url);
+
+        // Assert
+
+        $this->assertSame( Response::HTTP_FOUND, $client->getResponse()->getStatusCode() );
+
+    }
+
+
+
+
+
+
+
+    public function testProductRedirectingPageContent2() {
+
+        // Arrange
+
+        $url = '/product/';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        $searchText = 'Seafood index';
+
+        // Act
+
+        $client->request($httpMethod, $url);
+
+
+
+        // Assert
+
+        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
+    }
+
+
+
+    public function testProductPageContent() {
+
+        // Arrange
+
+        $url = '/product/lamb chop';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        $searchText = 'lean';
+
+        // Act
+
+        $client->request($httpMethod, $url);
+
+
+
+        // Assert
+
+        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
+    }
+
+
 
     public function testNewProductPageContent() {
+
         // Arrange
+
         $url = '/product/lamb chop';
+
         $httpMethod = 'GET';
+
         $client = static::createClient();
+
         $searchText = 'title';
+
         // Act
+
         $client->request($httpMethod, $url);
 
+
+
         // Assert
+
         $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
     }
+
+
 
     public function testEditProductPageContent() {
+
         // Arrange
-        $url = '/product/shark/edit';
+
+        $url = '/product/lamb chop/edit';
+
         $httpMethod = 'GET';
+
         $client = static::createClient();
+
         $searchText = 'title';
+
         // Act
+
         $client->request($httpMethod, $url);
 
-        // Assert
-        $this->assertContains( $searchText, $client->getResponse()->getContent() );
-    }
 
+
+        // Assert
+
+        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
+    }
 
 }
--- tests/Controller/ReviewControllerTest.php
+++ tests/Controller/ReviewControllerTest.php
@@ -20,16 +20,23 @@ class ReviewcontrollerTest extends WebTestCase
 
 
     public function testReviewRedirectingPageContent() {
-        // Arrange
         $url = '/review/';
         $httpMethod = 'GET';
         $client = static::createClient();
-        $searchText = 'Redirecting';
-        // Act
-        $client->request($httpMethod, $url);
 
-        // Assert
-        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+        $expectedResult = Response::HTTP_OK;
+        $expectedContent='';
+        $expectedContentToLowerCase = strtolower($expectedContent);
+
+        //act
+        $client->request($httpMethod, $url);
+        $statusCode = $client->getResponse()->getStatusCode();
+        $resultContent = $client->getResponse()->getContent();
+        $resultContentToLowerCase = strtolower($resultContent);
+
+        //assert
+        $this->assertSame($expectedResult, $statusCode);
+        $this->assertContains($expectedContentToLowerCase, $resultContentToLowerCase);
     }
 
     public function testSpecificReviewPageContent() {
@@ -50,7 +57,7 @@ class ReviewcontrollerTest extends WebTestCase
         $url = '/review/new';
         $httpMethod = 'GET';
         $client = static::createClient();
-        $searchText = 'author';
+        $searchText = 'Redirecting';
         // Act
         $client->request($httpMethod, $url);
 
@@ -63,12 +70,140 @@ class ReviewcontrollerTest extends WebTestCase
         $url = '/review/shark/edit';
         $httpMethod = 'GET';
         $client = static::createClient();
-        $searchText = 'author';
+        $searchText = 'Redirecting';
         // Act
         $client->request($httpMethod, $url);
 
         // Assert
         $this->assertContains( $searchText, $client->getResponse()->getContent() );
+    }
+
+    public function testReviewPageResponseCodeMoved1() {
+
+        // Arrange
+
+        $url = '/review';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        // Assert
+
+        $client->request($httpMethod, $url);
+
+        // Assert
+
+        $this->assertSame( Response::HTTP_MOVED_PERMANENTLY, $client->getResponse()->getStatusCode() );
+
+    }
+
+
+
+
+
+
+
+    public function testReviewRedirectingPageContent1() {
+
+        // Arrange
+
+        $url = '/review/';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        $searchText = 'Review index';
+
+        // Act
+
+        $client->request($httpMethod, $url);
+
+
+
+        // Assert
+
+        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
+    }
+
+
+
+    public function testSpecificReviewPageContent1() {
+
+        // Arrange
+
+        $url = '/review/shark';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        $searchText = 'nice';
+
+        // Act
+
+        $client->request($httpMethod, $url);
+
+
+
+        // Assert
+
+        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
+    }
+
+
+
+    public function testNewReviewPageContent1() {
+
+        // Arrange
+
+        $url = '/review/new/';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        $searchText = 'author';
+
+        // Act
+
+        $client->request($httpMethod, $url);
+
+
+
+        // Assert
+
+        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
+    }
+
+
+
+    public function testEditReviewPageContent1() {
+
+        // Arrange
+
+        $url = '/review/shark/edit';
+
+        $httpMethod = 'GET';
+
+        $client = static::createClient();
+
+        $searchText = 'author';
+
+        // Act
+
+        $client->request($httpMethod, $url);
+
+
+
+        // Assert
+
+        $this->assertContains( $searchText, $client->getResponse()->getContent() );
+
     }
 
 
